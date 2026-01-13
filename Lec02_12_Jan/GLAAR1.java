ba65d74f158e768f63dd4a552a16b8023927f954
Wasif97488 <w4wasifrock@gmail.com>
2026-01-13 19:32:02 +0530

    Simple Program using Java through Notepad and Identifiers

---
+++ Lec02_12_Jan/GLAAR1.java
@@ -0,0 +1,7 @@
+class GLAAR1
+{
+public static void main(string args[]);
+{
+system.out.println("Welcome to GLA");
+}
+}
---
+++ Lec03_13_Jan/Identifiers.java
@@ -0,0 +1,20 @@
+class Identifiers
+{
+public static void main(String args[])
+{
+int abc= 123;
+int abc123 = 123;
+//int 123abc= 123;
+int abc123_ = 123;
+int _abc123= 123;
+int abc123$ = 12345;
+int $abc123 = 1342;
+//int abc123# = 1234;
+int absvdhdgdjdgdkdh353425625 = 63536;
+// do = 223;
+//int if = 123;
+//int for = 123;
+//int break = 123;
+System.out.println(abc);
+}
+}
